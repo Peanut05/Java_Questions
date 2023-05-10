68844ad09e8a8d980bd04761c3b0ae77f98e0556
krushna Gavali <krushnagawali744@gmail.com>
2023-05-10 23:59:29 +0530

    Create 05_Names.java

---
+++ 05_Names.java
@@ -0,0 +1,9 @@
+package Homework2;
+
+public class Names {
+	// Multi Level Inheritance
+	String emp1Name = "Aman Chandan";
+	String emp2Name = "Ram Patil";
+	String emp3Name = "Shekhar Bhoi";
+	
+}
