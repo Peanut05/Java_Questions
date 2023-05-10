b9ca28d9baf93e37346a94c49c2381a2ad6e10d3
krushna Gavali <krushnagawali744@gmail.com>
2023-05-11 00:49:18 +0530

    Create 12_MethodShodowing.java

---
+++ 12_MethodShodowing.java
@@ -0,0 +1,13 @@
+package Q3Polymorphism;
+
+public class MethodShadowing {
+	
+	// Method Shodowing ....
+	
+	public void printHello() {
+		System.out.println("Hello from Parent .....");
+	}
+	int i1 = 100;
+	
+
+}
