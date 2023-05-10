72dccc77666647316093d44a9067aa0fb003538b
krushna Gavali <krushnagawali744@gmail.com>
2023-05-11 02:21:36 +0530

    Create 22_DiamondProblem.java

---
+++ 22_DiamondProblem.java
@@ -0,0 +1,18 @@
+package DiamondProblem;
+
+//solveing diamond problem using interface ....
+
+public class DiamondDrivers implements Diamond,Diamond2{
+	
+	public void m1() {
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		
+	 //m1();
+		
+		
+	}
+
+}
