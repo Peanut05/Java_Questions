614b8511b9fbf6b5eab1e85e5ee1ffb45b82e1d2
krushna Gavali <krushnagawali744@gmail.com>
2023-05-10 23:09:19 +0530

    Create 01_Encapsulation.java

---
+++ 01_Encapsulation.java
@@ -0,0 +1,31 @@
+package Homework2;
+
+public class Encapsulation {
+	
+	private String name;
+	private double price;
+	private String speciallity;
+	
+	// Designing Getter and Setter method ...
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getSpeciallity() {
+		return speciallity;
+	}
+	public void setSpeciallity(String speciallity) {
+		this.speciallity = speciallity;
+	}	
+
+}
